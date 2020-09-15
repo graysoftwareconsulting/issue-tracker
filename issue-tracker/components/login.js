a42fb116d932062e16d0d9b6bd710595e9572096
Awood549 <Austin.wood1776@live.com>
2020-09-14 17:01:05 -0700

    connected first file, the first of many

---
+++ issue-tracker/components/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Login(props){
+
+  let state = {
+    title:'Connection!'
+
+  }
+  return(
+    <h1>We've got {state.title} </h1>
+    )
+
+}
+
+export default Login
