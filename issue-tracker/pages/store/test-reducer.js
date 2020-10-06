e4c084b4378d179bdc125072eb48e41da58a6e02
Awood549 <Austin.wood1776@live.com>
2020-10-06 10:37:38 -0700

    login works the first time but has a in the reducer that needs to be squashed

--- issue-tracker/pages/store/test-reducer.js
+++ issue-tracker/pages/store/test-reducer.js
@@ -7,6 +7,7 @@ const todos = (state = initialState, action) => {
   switch (action.type) {
     case 'LOGIN_UPDATED':
       return(state.loggedIn === true ? state.loggedIn=false : state.loggedIn = true)
+      
     case 'ADD_TODO':
       return [
         ...state,
