479a375cd51a6684acd37e8d6c540ab0c9021171
Awood549 <Austin.wood1776@live.com>
2020-10-07 10:32:13 -0700

    building navbar and commiting start of tasks page

---
+++ issue-tracker/pages/tasks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function tasks(props){
+
+  let state = {
+  }
+
+  return (
+    <>
+
+    <div name='todo'>
+
+    </div>
+    <div name = 'inProgress'>
+
+    </div>
+    <div name = 'done'>
+
+    </div>
+    </>
+    
+  )
+
+}
